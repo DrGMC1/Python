b1c59bd0a0b28a3cbd4ed8ea50f8696cf58d1916
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-09-10 16:55:44 -0400

    Add files via upload

---
+++ Arithmetic Operators/ArithmeticOperators.py
@@ -0,0 +1,39 @@
+# Addition (+)
+a = 10
+b = 20
+c= 3
+
+print(a + b)
+print(10 + 30)
+print()
+
+# Subtraction
+print(b - a)
+print()
+
+# Multiplication
+print(a * b)
+print()
+
+# Division
+print(b / a)
+print()
+
+# Modulo
+print(25 % a)
+print()
+
+# Floor division(//)
+print(10.24 / 2)
+print(10.24 // 2)
+print()
+
+print(-3 / 2)
+print(-3 // 2)
+print()
+
+# Exponent operator or power operator (**)
+print(10 ** 2)
+print (10 ** c)
+
+
