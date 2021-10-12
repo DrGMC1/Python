2649d264b597514621b9868f1a2a54fe9a97ce26
DrGMC <47373280+DrGMC@users.noreply.github.com>
2021-10-12 13:29:27 -0400

    Add files via upload

---
+++ two_functions.py
@@ -0,0 +1,19 @@
+# This program has two functions. First we
+# define the main function.
+
+def main():
+    print('I have a message for you.')
+    message()
+    print('Goodbye class!')
+    
+# Next we define the message function.
+def message():
+    print('This is your favorite class ever')
+    print("That's correct, ITS 250 Programming I.")
+
+# Call the main function ONLY.
+main()
+
+
+
+
---
+++ void and value returning functions.py
@@ -0,0 +1,22 @@
+###value returning fuction
+##
+##def saySomething(thisIsaParameter):
+##        print(thisIsaParameter)
+##
+###function call
+##saySomething("This Is An Argument. First Call")
+##saySomething("This Is An Argument again invoking the respective fuction, second call")
+##
+
+
+# a nice example here of a void fuction
+def mainExample():
+        subj = "ITS"
+        course = 250
+        more = " the best class ever @ Campbell"
+        print (subj, course, more)
+
+#call
+mainExample()
+mainExample()
+
